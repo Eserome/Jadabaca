34ea7f1b3988216c735e6c07f7e623fed94134c0
Sergio <sergioromeroalarcon2b@gmail.com>
2023-03-21 12:56:35 +0100

    La realidad es cierta
    Signed-off-by: Sergio <sergioromeroalarcon2b@gmail.com>

---
+++ gitProFlex/src/gitProFlex/gitProFlex.java
@@ -0,0 +1,14 @@
+package gitProFlex;
+
+public class gitProFlex {
+
+	static int habilidades=99;
+	
+	int mostrarHabilidades() {
+		return habilidades;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("The attraction wave of technology provides truth & faith");
+	}
+}
